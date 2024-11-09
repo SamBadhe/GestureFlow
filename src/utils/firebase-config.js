0c1e54a47abd1dc0dc41640cddfb3b6c74d479b8
SamBadhe <samarthbadhe03@gmail.com>
2024-11-10 02:46:31 +0530

    Updated the Profile page

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -4,13 +4,24 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDC9vr3ScGFdpxZAqrzKyeaVlLMkocmsDo",
-  authDomain: "react-app-41e3e.firebaseapp.com",
-  projectId: "react-app-41e3e",
-  storageBucket: "react-app-41e3e.firebasestorage.app",
-  messagingSenderId: "382693113743",
-  appId: "1:382693113743:web:a7031117c204ef5bfe15fa",
-  measurementId: "G-3G06G03EF9",
+
+  //Account with storage enabled
+  apiKey: "AIzaSyB0jJelgoptbmztOfslGydaP1br8tigLmc",
+  authDomain: "flutter-app-1811.firebaseapp.com",
+  projectId: "flutter-app-1811",
+  storageBucket: "flutter-app-1811.appspot.com",
+  messagingSenderId: "10805628758",
+  appId: "1:10805628758:web:1f007f83f8cd5db19250f7"
+
+  // Account without Storage enabled.
+  // apiKey: "AIzaSyDC9vr3ScGFdpxZAqrzKyeaVlLMkocmsDo",
+  // authDomain: "react-app-41e3e.firebaseapp.com",
+  // projectId: "react-app-41e3e",
+  // storageBucket: "react-app-41e3e.firebasestorage.app",
+  // messagingSenderId: "382693113743",
+  // appId: "1:382693113743:web:a7031117c204ef5bfe15fa",
+  // measurementId: "G-3G06G03EF9",
+  
 };
 
 const app = initializeApp(firebaseConfig);
